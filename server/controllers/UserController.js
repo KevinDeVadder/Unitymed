b9e480b5556976f1ef43f61ce8248d57c3170302
De Vadder Kevin <devadderkevin@gmail.com>
2019-04-20 12:57:02 +0300

    Initialised the client and the server

---
+++ server/controllers/UserController.js
@@ -0,0 +1,72 @@
+const UserModel = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mail = require('../helpers/mailconfig')
+
+module.exports = {
+    async create(req, res, next) {
+        // console.log(req.body)
+        try{
+            const user = await UserModel.create({ name: req.body.name, email: req.body.email, password: req.body.password })
+            const emailToken = jwt.sign(
+                {user: user._id}, 
+                req.app.get('secretKey'),
+                { expiresIn: '1h' }
+            );
+            const url = `http://localhost:4000/api/confirm/${emailToken}`;
+            mail.transporter.sendMail({
+                from: 'Unitymed <devadderkevin@gmail.com>',
+                to: user.email,
+                subject: 'Confirm account!',
+                html: 'Please click <a href="' + url +'">' + url + '</a>'
+            }).then(
+                () =>{
+                    res.send({"status": "email sent"})
+                }
+            ).catch(next)
+        }
+        catch(err){
+            next(err)
+        }
+    },
+   async authenticate(req, res, next) {
+       try{
+            const user = await UserModel.findOne({email:req.body.email});
+            if(bcrypt.compareSync(req.body.password, user.password)) {
+                if (user.confirmed == false){
+                    res.status(401).send({"error": "user not yet confirmed"})
+                }
+                else{
+                const toSend = {
+                    name: user.name,
+                    email: user.email,
+                    admin: user.admin,
+                    completedQuizzes: user.completedQuizzes
+                }
+                const token = jwt.sign({id: user._id, admin: user.admin}, req.app.get('secretKey'), { expiresIn: '1h' });
+                res.json({status:"success", message: "user found!!!", data:{user: toSend, token:token}});
+                }
+            }
+            else{
+            res.json({status:"error", message: "Invalid email/password!!!", data:null});
+            }
+       }
+       catch(err){
+           next(err)
+       }
+
+    },
+    confirm(req, res, next){
+        jwt.verify(req.params.token, req.app.get('secretKey'), function (err, decoded) {
+            if (err) {
+                res.status(400).json({ status: "error", message: err.message, data: null });
+            } else {
+                // confirm user
+                UserModel.findByIdAndUpdate({_id: decoded.user}, {confirmed: true}).then(
+                    res.send({message: "success!!!"})
+                ).catch(next)
+            }
+        });
+        return res.redirect('http://localhost:8080/login');
+    }
+   }
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = require('./routes/api')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+
+app.set('secretKey', 'CHEIESECRETA');
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/unitymed', {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+//error handling
+app.use((err, req, res, next) => {
+    res.status(422).send({ error: err.message });
+});
+
+app.listen(4000, ()=>{
+    console.log('Server running on port 4000')
+})
